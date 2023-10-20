09bfc564524ed684a06894c6ebbe58faff860317
Animesh245 <bdanimesh245@gmail.com>
2023-10-21 02:01:39 +0600

    Initial commit

---
+++ src/test/java/com/animesh245/emppay/EmpPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animesh245.emppay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
